8615637428942dab993202a70e010438ee5658e1
Jarno <43700421+jarnodev@users.noreply.github.com>
2020-03-31 14:23:01 +0200

    Get rid of settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        Cache::forever('settings', \App\Models\Settings::all());
     }
 }
